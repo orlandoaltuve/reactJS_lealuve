6d99ba5689ef615c001e897b6a663297d6bba8df
Orlando Altuve <orlandoaltuve20@gmail.com>
2022-08-10 22:23:42 -0300

    mi primera aplicacion en react

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Orlando Altuve. Mi primera aplicacion en  ReactJs
         </p>
         <a
           className="App-link"
